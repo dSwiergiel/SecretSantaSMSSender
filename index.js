d3b2f4081fd6a5046e7e4051ee9fd3d7bd0f6c05
Deven Swiergiel <dswiergiel@albany.edu>
2018-12-08 19:52:37 -0500

    Finalizes code

--- index.js
+++ index.js
@@ -12,19 +12,17 @@ var to, text;
 secretSanta();
 
 async function secretSanta() {
+
   shuffle(Santa);
 
   while (Santa.length != 0) {
-    if (
-      Santa[0].contactName.includes(Santee[0].contactName) ||
-      Santee[Santa.length - 1].contactName.includes(
-        Santa[Santa.length - 1].contactName
-      )
-    ) {
+
+     // shuffle if Santee is also Santa and vice versa
+    if (Santa[0].contactName.includes(Santee[0].contactName) || Santee[Santa.length - 1].contactName.includes(Santa[Santa.length - 1].contactName)) {
+      
       shuffle(Santa);
       shuffle(Santee);
     } else {
-
 
        to = Santa[0].contactNumber;
 
@@ -37,15 +35,18 @@ async function secretSanta() {
 
       await delay(5000);
 
+      // should log error or success response here but did not for sake of time
       await nexmo.message.sendSms(from, to, text);
 
+      // remove current Santa and Santee form each list
       Santa.shift();
       Santee.shift();
+
       console.log(Santa.length + 1);
     }
   }
 }
-
+// randomizes the lists
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
